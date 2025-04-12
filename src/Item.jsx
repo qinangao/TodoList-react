7dbfdd8ffa949a76f5f8c6fad595f73d7d7bfc11
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-12 22:53:49 +0100

    add Item component

---
+++ src/Item.jsx
@@ -0,0 +1,15 @@
+export default function Item({ task, onDeleteTask, onCheckTask }) {
+  return (
+    <li>
+      <div className="content">
+        <input type="checkbox" onClick={() => onCheckTask(task.id)} />
+        <span className={task.checked ? "checked" : ""}>
+          {task.description}
+        </span>
+      </div>
+      <button className="btn--delete" onClick={() => onDeleteTask(task.id)}>
+        <img src="/delete.svg" alt="delete-svg" />
+      </button>
+    </li>
+  );
+}
