7b35f521ad1857a1b6b3fbe3f9d0bc12a565cb5e
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-12 23:24:48 +0100

    add stats

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import TodoApp from "./TodoApp";
 import Footer from "./Footer";
+import { useState } from "react";
 
 // const list = [
 //   { id: 1, description: "Learn React", checked: false },
@@ -7,10 +8,45 @@ import Footer from "./Footer";
 //   { id: 3, description: "Cooking", checked: true },
 // ];
 function App() {
+  const [tasks, setTasks] = useState([]);
+  function handleAddTask(task) {
+    setTasks((tasks) => [...tasks, task]);
+  }
+
+  function handleDeleteTask(id) {
+    setTasks((tasks) => tasks.filter((task) => task.id !== id));
+  }
+
+  function handleCheckTask(id) {
+    setTasks((tasks) =>
+      tasks.map((task) =>
+        task.id === id ? { ...task, checked: !task.checked } : task
+      )
+    );
+  }
   return (
     <div className="wrapper">
-      <TodoApp />
+      <Stats tasks={tasks} />
+      <TodoApp
+        tasks={tasks}
+        onAddTask={handleAddTask}
+        onDeleteTask={handleDeleteTask}
+        onCheckTask={handleCheckTask}
+      />
       <Footer />
+    </div>
+  );
+}
+
+function Stats({ tasks }) {
+  // const numChecked=1
+  return (
+    <div>
+      <h2>
+        {tasks.length === 0
+          ? "😴 No tasks yet! Time to plan something awesome 🗓️🌟"
+          : `You have ${tasks.length} unfinished tasks  📝⏳`}
+      </h2>
     </div>
   );
 }
