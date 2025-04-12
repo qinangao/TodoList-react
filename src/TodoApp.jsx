b341773de23bdb2f0ae947fb75feb18e2b46f1e8
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-12 22:54:17 +0100

    add todo list app component

---
+++ src/TodoApp.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Title from "./Title";
+import Form from "./Form";
+import List from "./List";
+
+// const list = [
+//   { id: 1, description: "Learn React", checked: false },
+//   { id: 2, description: "Learn JS", checked: false },
+//   { id: 3, description: "Cooking", checked: true },
+// ];
+function TodoApp() {
+  const [tasks, setTasks] = useState([]);
+
+  function handleAddTask(task) {
+    setTasks((tasks) => [...tasks, task]);
+  }
+
+  function handleDeleteTask(id) {
+    setTasks((tasks) => tasks.filter((task) => task.id !== id));
+  }
+
+  function handleCheckTask(id) {
+    setTasks((tasks) =>
+      tasks.map((task) =>
+        task.id === id ? { ...task, checked: !task.checked } : task
+      )
+    );
+  }
+  return (
+    <div className="app">
+      <Title />
+      <Form onAddTask={handleAddTask} />
+      <List
+        tasks={tasks}
+        onDeleteTask={handleDeleteTask}
+        onCheckTask={handleCheckTask}
+      />
+    </div>
+  );
+}
+
+export default TodoApp;
