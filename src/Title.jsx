c19e78619e67bb16b0ccaaaad01fcffe768bf1ce
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-12 22:54:04 +0100

    add title component

---
+++ src/Title.jsx
@@ -0,0 +1,11 @@
+export default function Title() {
+  return (
+    <div className="title--container">
+      <div className="title">
+        <img src="/todo.png" alt="logo" />
+        <h1>Todo List</h1>
+      </div>
+      <p>Little tasks, big wins!🐻☕📋</p>
+    </div>
+  );
+}
