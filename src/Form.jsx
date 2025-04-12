d38f0181ac0c03c386be2dc660372a590cc5720c
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-12 22:53:43 +0100

    add form component

---
+++ src/Form.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function Form({ onAddTask }) {
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(description.length);
+    if (!description) return;
+    if (description.length > 25) {
+      alert("Input exceeds limit. Try again.");
+      setDescription("");
+      return;
+    }
+
+    const newTask = { description, checked: false, id: Date.now() };
+
+    setDescription("");
+    onAddTask(newTask);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="task--input"
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="btn-add">Add</button>
+      </form>
+    </div>
+  );
+}
