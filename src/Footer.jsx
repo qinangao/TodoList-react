40462532ba67894be9072b47915cf05dcdbe334d
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-12 22:53:36 +0100

    add footer component

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Designed and developed by Qinan Gao — a small project to practice React.
+        ❤️
+      </p>
+    </footer>
+  );
+}
